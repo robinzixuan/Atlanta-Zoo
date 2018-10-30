3b491e4441b816002138c024a70f6d47e055a3df
robinzixuan <luohaozh@gmail.com>
2018-10-30 15:40:43 -0400

    v2

--- phrase 3.py
+++ phrase 3.py
@@ -1,19 +1,28 @@
 import os
 import MySQLdb
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
 
 
-
-def opendb():
-    db = MySQLdb.connect(host="academic-mysql.cc.gatech.edu",user="cs4400_group32",password="AXtzKclB",db="cs4400_group32" )
+def opendb(db,table):
     try:
         cursor = db.cursor()
-        cursor.execute("SELECT * from User")
+        cursor.execute("SELECT * from "+table)
         data = cursor.fetchone()
-        for i in data:
-            print(i)
+        print('success')
+        return data
     except:
-         print('Error')
+         print('Error') 
          
-            
-            
-opendb()
+def main():
+    db = MySQLdb.connect(host="academic-mysql.cc.gatech.edu",user="cs4400_group32",password="AXtzKclB",db="cs4400_group32" )
+    data=opendb(db,"User")
+    
+    
+    db.close()
+    
+    
+    
+    
+main()
